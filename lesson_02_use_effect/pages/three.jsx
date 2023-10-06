8c875f699b53fd1f9a13aa998fcae34c887937e7
Цэрэн янжин Бүдээхүү <LP230127@G301-CC0182.local>
2023-10-06 19:25:09 +0800

    object nemsen contact ajilsan

--- lesson_02_use_effect/pages/three.jsx
+++ lesson_02_use_effect/pages/three.jsx
@@ -1,11 +1,25 @@
 import React from "react";
-import MoreThree from "@/components/more/moreThree";
-const More = () => {
+import { Canvas } from "@react-three/fiber";
+import { BoxGeometry } from "three";
+import { OrbitControls } from "@react-three/drei";
+import styled from "styled-components";
+const Three = () => {
+  const Container = styled.div`
+    background-color: #effec7;
+  `;
   return (
-    <div>
-      <MoreThree />
-    </div>
+    <Container>
+      <Canvas>
+        <directionalLight position={[1, 1, 1]} />
+        <mesh>
+          <boxGeometry args={[3, 3, 3]} />
+          <OrbitControls />
+
+          <meshStandardMaterial color="pink" />
+        </mesh>
+      </Canvas>
+    </Container>
   );
 };
 
-export default More;
+export default Three;
